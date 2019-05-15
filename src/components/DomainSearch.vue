af379dfd8bd7ea6a027223a45b473b74da670097
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2019-05-15 00:25:32 -0500

    feat: route validation and elements centred in small screens

--- src/components/DomainSearch.vue
+++ src/components/DomainSearch.vue
@@ -1,9 +1,14 @@
 <template>
+  <flex-row align-h="center">
   <b-nav-form v-on:submit.prevent="findHostInfo()">
+    <flex-row align-h="center">
     <b-form-input
       size="sm"
       class="mr-sm-2"
+      :state="validInput"
       placeholder="Search"
+      v-b-tooltip
+      :title="validInput ? '' : 'Write a valid URL'"
       v-model="host"
     ></b-form-input>
     <b-button
@@ -13,20 +18,35 @@
       type="button"
       >Search</b-button
     >
+    </flex-row>
+
   </b-nav-form>
+  </flex-row>
 </template>
 
 <script>
 import { ActionTypes } from "../store/ActionTypes";
+import { validURL } from "../common/util";
+import FlexRow from "vue-flex/lib/components/FlexRow";
 export default {
   name: "DomainSearch",
-  data: function() {
+    components: {FlexRow},
+    data: function() {
     return {
       host: ""
     };
   },
+    computed: {
+      validInput() {
+          if(this.host==="") return true;
+          return validURL(this.host);
+      }
+    },
   methods: {
     findHostInfo() {
+        if(!this.validInput || this.host==="") {
+            return
+        }
       let loader = this.$loading.show();
       this.$store
         .dispatch(ActionTypes.GET_HOST_INFO, this.host)
