fd23e853aded44a8c7b056a301f830393c9bcfca
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2019-05-19 18:22:56 -0500

    refact: list last domain searched now extracts the page from params

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <navbar></navbar>
     <div id="nav">
       <router-link to="/">Home</router-link>&nbsp;|
-      <router-link to="/lastSearched">Last Searched</router-link>&nbsp;|
+      <router-link :to="{ name: 'lastSearched', params: { page: 1 }}">Last Searched</router-link>&nbsp;|
       <router-link to="/about">About</router-link>
     </div>
     <router-view class="router-view" />
--- src/router.js
+++ src/router.js
@@ -15,7 +15,7 @@ export default new Router({
       component: Home
     },
     {
-      path: "/lastSearched",
+      path: "/lastSearched/:page",
       name: "lastSearched",
       component: LastSearchedDomains
     },
